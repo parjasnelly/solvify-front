43f3ceb413e6a863e6e3770448818fa84ec0a8c1
Parjasnelly A <63004038+parjasnelly@users.noreply.github.com>
2023-05-29 21:21:56 -0300

    Implements Sign in with Google WIP

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { SocialAuthService } from "@abacritt/angularx-social-login";
+import { SocialUser } from "@abacritt/angularx-social-login";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.sass']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit  {
+
+  user: SocialUser | undefined;
+  loggedIn: boolean | undefined;
+
+  constructor(private authService: SocialAuthService) { }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {SocialLoginModule, SocialAuthServiceConfig, GoogleSigninButtonModule} from '@abacritt/angularx-social-login';
+import {GoogleLoginProvider} from '@abacritt/angularx-social-login';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './Pages/login/login.component';
-import { HomeComponent } from './Pages/home/home.component';
-import { DashboardComponent } from './Pages/dashboard/dashboard.component';
-import { HeaderComponent } from './Components/header/header.component';
-import { CreateQuestionComponent } from './Pages/create-question/create-question.component';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './Pages/login/login.component';
+import {HomeComponent} from './Pages/home/home.component';
+import {DashboardComponent} from './Pages/dashboard/dashboard.component';
+import {HeaderComponent} from './Components/header/header.component';
+import {CreateQuestionComponent} from './Pages/create-question/create-question.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,8 +20,25 @@ import { CreateQuestionComponent } from './Pages/create-question/create-question
     HeaderComponent,
     CreateQuestionComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, SocialLoginModule, GoogleSigninButtonModule],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(
+            '814334065115-sk0m67fdt5jq2shd3jv6qgb5ed88k469.apps.googleusercontent.com'
+          )
+        }
+      ],
+      onError: (err) => {
+        console.error(err);
+      }
+    } as SocialAuthServiceConfig,
+  }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
